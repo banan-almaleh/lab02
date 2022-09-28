9b0a7d4f5e5f968dbbb088750f69b576783807c0
Banan Almaleh <banan.almaleh@mycit.ie>
2022-09-28 15:45:13 +0100

    updated version of DartsMatch

--- domain/darts_match.py
+++ domain/darts_match.py
@@ -4,3 +4,21 @@ class DartsMatch:
         self.type = type
         self.player1 = player1
         self.player2 = player2
+
+    def __init__(self):
+        self.active = True
+        self.players = []
+        self.last_players_index = -1
+        self.visits = []
+        self.winning_num_darts = -1
+        self.winning_player_index = -1
+
+    def register_player(self, username):
+        if username not in self.players:
+            index = len(self.players)
+            self.players.append(username)
+            self.visits.append([])
+            return index
+        else:
+            return -1
+
